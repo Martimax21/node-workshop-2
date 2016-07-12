d7d935ff3050cd28d7797d36c7fa5b1709281cd4
Maxime Martin <maxime.martin123@gmail.com>
2016-07-12 23:28:29 +0000

    getsynonymsExercice

---
+++ get-synonyms.js
@@ -0,0 +1,49 @@
+// Creating the program:
+// Create a file get-synonyms.js at the root of your project
+// Import your module and create an instance using your API key
+// Prompt the user for a word
+// Using your API, retrieve the synonyms/antonyms/etc. for the input word
+// If everything goes well, display all the results to the user in a nice way
+// Hint: to display the results in a nice way, a few NPM modules could be useful, including but not limited to:
+// colors
+
+// cli-table
+
+// node-emoji
+
+// Add/commit/push
+var colors = require('colors');
+var emoji = require('node-emoji');
+var prompt = require('prompt')
+var request = require('request');
+var getSyns = require('./library/synonyms.js');
+
+var api = "591209d8c9a08977a6a17cf124c585de";
+
+
+
+prompt.get('synonymsofword', function(err, answers) {
+    if (err) {
+        console.log('there was an error');
+    }
+    else {
+        var synKey = new getSyns.SynonymApi(api);
+        synKey.getSynonyms(answers.synonymsofword, function(err, res) {
+            if (err) {
+                console.log(colors.rainbow("The word is not our the dictionnary, this is not urban dictionnary!"));
+            }
+            else {
+                 if (res.noun){
+                    console.log(res.noun.syn);
+                }
+                if (res.verb){
+                    console.log(res.verb.syn);
+                }
+                if (res.adjective){
+                    console.log(res.adjective.syn);
+                }
+            }
+        });
+
+    }
+});
